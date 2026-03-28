d7d252aca6ba00315accf10d3cea6ee5ff4f4f45
Bibounai <74238266+Bibounai@users.noreply.github.com>
2026-03-28 23:12:02 +0100

    Reset DB schema on startup so ChirpStack migrations run clean

--- start.sh
+++ start.sh
@@ -7,4 +7,8 @@ envsubst < /etc/chirpstack/chirpstack.toml.template > /etc/chirpstack/chirpstack
 # Inject $PORT into nginx config (only $PORT, leave nginx variables intact)
 envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+# Reset DB schema so ChirpStack migrations always run clean
+echo "Resetting PostgreSQL schema..."
+psql "${DATABASE_URL}" -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" || true
+
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
